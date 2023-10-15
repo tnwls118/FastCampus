5cd66ec14dd250bbade70e429e2b92d3a73270c9
tnwls118 <tnrruddl1220@gmail.com>
2023-10-15 21:24:41 +0900

    Fastcampus 실습11

--- src/ArrayBasicTest.java
+++ src/ArrayBasicTest.java
@@ -2,7 +2,7 @@ public class ArrayBasicTest {
     public static void main(String[] args) {
         //정수 5개를 저장 할 배열을 생성하고 모두 10을 저장하여 합산하시오.
 
-        int[] 정수 = new int[5];
+        int[] 정수 = new int[50];
 
         int sum = 0;
         for(int i =0; i<정수.length; i++) {
