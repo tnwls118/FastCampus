a1264e96f8dc58d5f5206c0fd20c706ff3d4a51d
tnwls118 <tnrruddl1220@gmail.com>
2023-10-18 17:01:37 +0900

    Fastcampus 실습15 - 이차원 배열 및 가변길이

--- src/TwoDimArrayTest.java
+++ src/TwoDimArrayTest.java
@@ -1,23 +1,33 @@
 public class TwoDimArrayTest {
     public static void main(String[] args) {
-        //2행 4열의 메트릭스 생성
-        //Q.행(row),열(column)로 데이터 저장하기
-        int[][] a = new int[2][4];
-        a[0][0] = 10;
-        a[0][1] = 20;
-        a[0][2] = 30;
-        a[0][3] = 40;
-        a[1][0] = 50;
-        a[1][1] = 60;
-        a[1][2] = 70;
-        a[1][3] = 80;
-        //row-major 메모리 확장
+//        //2행 4열의 메트릭스 생성
+//        //Q.행(row),열(column)로 데이터 저장하기
+//        int[][] a = new int[][]{{10, 20, 30, 40}, {50, 60, 70, 80}};
+//        //row-major 메모리 확장
+//
+//        for (int i = 0; i < a.length; i++) {
+//            for (int j = 0; j < a[i].length; j++) {
+//                System.out.println(a[i][j]);
+//            }
+//        }
+//        //이중 for문 활용
 
-        for(int i =0; i<a.length; i++) {
-            for (int j = 0; j < a[i].length; j++) {
-                System.out.println(a[i][j]);
+        //Q.가변 길이 배열 만들기
+        int[][] b = new int[5][];
+
+        b[0] = new int[]{1,2};
+        b[1] = new int[]{3,4,5};
+        b[2] = new int[]{6,7,8,9};
+        b[3] = new int[]{10,11,12};
+        b[4] = new int[2];
+        b[4][0] = 13;
+        b[4][1] = 14;
+
+        for (int i = 0; i < b.length; i++) {
+            for (int j = 0; j < b[i].length; j++) {
+                System.out.println(b[i][j]);
             }
         }
-        //이중 for문 활용
+
     }
 }
