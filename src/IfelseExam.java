2307f57d6d0a3dcd32e4180f17ba5472902d87f1
tnwls118 <tnrruddl1220@gmail.com>
2023-10-21 22:09:17 +0900

    Fastcampus 실습22 - 다중 if문, switchCase문

--- src/IfelseExam.java
+++ src/IfelseExam.java
@@ -6,7 +6,7 @@ public class IfelseExam {
         System.out.print("날짜를 입력하세요(format: 2023) : ");
         int year = scan.nextInt();
 
-        if (((year % 4 == 0) && (year % 100 == 0)) || (year % 400 == 0)) {
+        if (((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0)) {
             System.out.println("입력된 년도는 윤년입니다.");
         } else {
             System.out.println("입력된 년도는 윤년이 아닙니다.");
