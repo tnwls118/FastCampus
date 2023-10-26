2c4b0760f6b4b800c2244bc1957a07eb8ef5a544
tnwls118 <tnrruddl1220@gmail.com>
2023-10-26 23:21:12 +0900

    Fastcampus 실습32 - 접근제어자 학습

---
+++ src/modal/AccessModifier.java
@@ -0,0 +1,9 @@
+package modal;
+
+public class AccessModifier {
+    public static void main(String[] args) {
+        memberDTO dto = new memberDTO();
+        dto.name = "시바견";
+        dto.phone = "010-0000-0000";
+    }
+}
---
+++ src/modal/memberDTO.java
@@ -0,0 +1,8 @@
+package modal;
+
+public class memberDTO {
+    public String name;
+    private int age;
+    public String phone;
+
+}
